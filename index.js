36b1d9fd89aa3a8fb88f0675c8ababf9d66b1091
Afrikhana <brianmurigi64@gmail.com>
2023-04-01 13:29:01 +0300

    completed assignment

--- index.js
+++ index.js
@@ -20,4 +20,57 @@ const allWagesFor = function () {
 
     return payable
 }
+function createEmployeeRecord(arr){
+    return{
+        firstName:arr[0],
+        familyName:[1],
+        title:[2],
+        payPerHour:[3],
+        timeInEvents:[],
+        timeOutEvents:[]
+    }
+}
+function createEmployeeRecords(arr){
+    let obj=[]
+    for(let record of arr){
+        obj.push(createEmployeeRecord(record))
+    }
+    return obj
+}
+function createTimeInEvent(time){
+    //her we are turning YYYY-MM-DD HHMM into y-m-d,h by using split(' ')
+    let newTime=time.split(' ')
+    let obj={type:"TimeIn", hour:parseInt(newTime[1]), date:newtime[0]}
+    this.timeInEvents.push(obj)
+    return this
+}
+function createTimeOutEvent(time){
+    //her we are turning YYYY-MM-DD HHMM into y-m-d,h by using split(' ')
+    let newTime=time.split(' ')
+    let obj={type:"TimeOut", hour:parseInt(newTime[1]), date:newtime[0]}
+    this.timeOutEvents.push(obj)
+    return this
+}
 
+function hoursWorkedOnDate(date){
+    let timeinobj=this.timeInEvents.find(dateobj=>dateobj.date===date).hour
+    let timeoutobj=this.timeInEvents.find(dateobj=>dateobj.date===date).hour
+
+    return (timeoutobj-timeinobj)/100
+}
+
+function wagesEarnedOnDate(date){
+    return hourseWorkedOnDate.call(this, date)*this.payPerHour
+}
+function findEmployeeByFirstName(arr,str){
+    let found= arr.find(rc=>rc.firstName===str)
+ return found
+}
+
+function calculatePayroll(collection) {
+    let payroll = 0;
+    for (let employee of collection) {
+      payroll += allWagesFor(employee);
+    }
+    return payroll;
+  }
